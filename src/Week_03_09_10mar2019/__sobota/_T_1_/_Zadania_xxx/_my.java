38ef6920a91575af1d4fa2c32dc614919621bba1
dd200 <ss2000@wp.pl>
2019-03-14 22:54:25 +0100

    sda24java_14_03_2019

--- src/Week_03_09_10mar2019/__sobota/_T_1_/_Zadania_xxx/_my.java
+++
@@ -1,4 +0,0 @@
-package Week_03_09_10mar2019.__sobota._T_1_._Zadania_xxx;
-
-public class _my {
-}
---
+++ src/Week_03_09_10mar2019/__sobota/_T_1_interfejsy/_Zadania_interfejsy/_my.java
@@ -0,0 +1,4 @@
+package Week_03_09_10mar2019.__sobota._T_1_interfejsy._Zadania_interfejsy;
+
+public class _my {
+}
